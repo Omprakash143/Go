0e0fa8dc2ba1368ff519b20d4ff12630dc96950c
Omprakash143 <prakash1431125669@gmail.com>
2022-03-21 21:43:36 +0530

    added session management code - alexedwards/scs package

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module GoProject
 
 go 1.17
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require (
+	github.com/alexedwards/scs/v2 v2.5.0
+	github.com/go-chi/chi/v5 v5.0.7
+)
--- main.go
+++ main.go
@@ -3,26 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello Home"))
-
+	ip := r.RemoteAddr
+	session.Put(r.Context(), "ip", ip)
 	_, _ = fmt.Println(fmt.Sprintf("Home - number of bytes is "))
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello About"))
+
+	ip := session.GetString(r.Context(), "ip")
+	if len(ip) == 0 {
+		w.Write([]byte("Hello About Not sure of Your ip"))
+	} else {
+		w.Write([]byte("Hello About Your ip is " + ip))
+	}
 	_, _ = fmt.Println(fmt.Sprintf("About - number of bytes is "))
 }
+
+var session *scs.SessionManager
+
 func main() {
 
 	mux := chi.NewRouter()
+
+	// sessions
+	session = scs.New()
+	session.Lifetime = time.Second * 30
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+
 	mux.Use(middleware.Recoverer)
 	mux.Use(MiddleWareTest)
+	mux.Use(SessionMidlleware)
+
 	mux.Get("/", Home)
 	mux.Get("/About", About)
 
@@ -36,4 +57,8 @@ func MiddleWareTest(hf http.Handler) http.Handler {
 		hf.ServeHTTP(w, r)
 		fmt.Println("Page served success")
 	})
+}
+
+func SessionMidlleware(hf http.Handler) http.Handler {
+	return session.LoadAndSave(hf)
 }
